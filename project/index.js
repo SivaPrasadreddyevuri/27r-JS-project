418e28d832cc88d95af6ed1032b766a413fda9d4
https://github.com/SivaPrasadreddyevuri/27r.git <evurisivaprasadreddy3@gmail.com>
2025-03-14 18:32:06 +0530

    formvalidations

--- project/index.js
+++ project/index.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-import { getAuth,signInWithEmailAndPassword,createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js'
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,57 +17,103 @@ const firebaseConfig = {
   storageBucket: "r-js-96bce.firebasestorage.app",
   messagingSenderId: "651768745906",
   appId: "1:651768745906:web:e65b7ef55d38ee44195eab",
-  measurementId: "G-YZNTNY08FM"
+  measurementId: "G-YZNTNY08FM",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initializing Authentication
-const auth=getAuth(app)
+// Initializing Authentication 
+const auth = getAuth(app);
 
-let signup=document.getElementById("sign-up_Form")
-let signin=document.getElementById("sign-in_Form")
-let btn=document.getElementById("do_signin")
-let btn2=document.getElementById("do_signup")
-let signup_title=document.getElementById("signup-title")
+let signup = document.getElementById("sign-up_Form");
+let signin = document.getElementById("sign-in_Form");
+let btn = document.getElementById("do_signin");
+let btn2 = document.getElementById("do_signup");
+let signup_title = document.getElementById("signup-title");
+let signupEmailError = document.getElementById("signupEmail_err");
+let signupPasswordError = document.getElementById("signupPassword_err");
+let signupEmail = document.getElementById("signupEmail");
+let signupPassword = document.getElementById("signupPassword");
 
-btn.addEventListener("click",()=>{
-signin.style.display="inline"
-signup.style.display="none"
-signup_title.style.display="none"
-})
-btn2.addEventListener("click",()=>{
-signin.style.display="none"
-signup.style.display="inline"
-signup_title.style.display="inline-block"
-})
-signup.addEventListener("submit",(e)=>{
-  e.preventDefault()
-  let name=e.target[0].value
-  let email=e.target[1].value
-  let password=e.target[2].value
-  createUserWithEmailAndPassword(auth,email,password)
-  .then((x)=>{
-       alert("Account Successfully Created")
-        signin.style.display="inline"
-        signup.style.display="none"
-        signup_title.style.display="none"
-  })
-  .catch((e)=>{alert("Error Creating Account / Email Already Registered"
-  )})
+//Form Inputs
+signupEmail.addEventListener("focus", () => {
+  signupEmail.style.boxShadow = "0px 0px 25px yellow";
+  signupEmail.style.border = "none";
+  // signupEmailError.style.display="none"
+});
+signupEmail.addEventListener("blur", () => {
+  signupEmail.style.boxShadow = "none";
+  signupEmailError.style.display = "none";
+});
+signupPassword.addEventListener("focus", () => {
+  signupPassword.style.boxShadow = "0px 0px 25px  brown";
+  signupPassword.style.border = "none";
+});
+signupPassword.addEventListener("blur", () => {
+  signupPassword.style.boxShadow = "none";
+});
+// Navigating operations:
+btn.addEventListener("click", () => {
+  signin.style.display = "inline";
+  signup.style.display = "none";
+  signup_title.style.display = "none";
+});
+btn2.addEventListener("click", () => {
+  signin.style.display = "none";
+  signup.style.display = "inline";
+  signup_title.style.display = "inline-block";
+});
+// After form Submition
+signup.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let name = e.target[0].value;
+  let email = signupEmail.value;
+  let password = signupPassword.value;
+  // Validation For Email
+  let emailpattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  let emailvalid = emailpattern.test(signupEmail.value);
+  if (!emailvalid) {
+    signupEmailError.innerText = "invalid Email!";
+    signupEmailError.style.color = "red";
+  } else {
+    signupEmailError.innerText = " ";
+  }
+  // Validation for Password
+  let passwordPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  let passwordValid = passwordPattern.test(signupPassword.value);
 
-})
+  if (!passwordValid) {
+    signupPasswordError.innerText =
+      "Password must include at least one uppercase letter, one lowercase letter, one number, and one special character.";
+    signupPasswordError.style.color = "red";
+  } else {
+    signupPasswordError.innerText = " ";
+  }
 
-signin.addEventListener("submit",(e)=>{
-e.preventDefault()
-let name=e.target[0].value
-let email=e.target[1].value
-signInWithEmailAndPassword(auth,name,email)
-.then((x)=>{
-    alert("Login Sucessfully Done")
-    location.replace("./home.html")
-})
-.catch((e)=>{alert("Invalid Credentials!!!! Sorryyyyy!")})
-
-})
-
+  if (emailvalid && passwordValid) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((x) => {
+        alert("Account Successfully Created");
+        signin.style.display = "inline";
+        signup.style.display = "none";
+        signup_title.style.display = "none";
+      })
+      .catch((e) => {
+        alert("Error Creating Account / Email Already Registered");
+      });
+  }
+});
+signin.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let name = e.target[0].value;
+  let email = e.target[1].value;
+  signInWithEmailAndPassword(auth, name, email)
+    .then((x) => {
+      alert("Login Sucessfully Done");
+      location.replace("./homepage.html");
+    })
+    .catch((e) => {
+      alert("Invalid Credentials!!!! Sorryyyyy!");
+    });
+});
